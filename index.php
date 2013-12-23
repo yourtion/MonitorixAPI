4a43cce8a1c5d5c8f35aa1c31dc58270deab1eab
Yourtion <yourtion@gmail.com>
2013-12-23 23:06:06 +0800

    Simple GET function

--- index.php
+++ index.php
@@ -3,4 +3,15 @@ include_once('core.php');
 include_once('function.php');
 //echo pageToJson("test.html");
 //echo pageToJson(getPageUrl());
-echo indexToJson(getUrl());
+//echo indexToJson(getUrl());
+if($_GET['act']=="page"){
+	if($_GET['when'] && $_GET['graph']){
+		echo pageToJson(getPageUrl($_GET['when'],$_GET['graph']));
+	}elseif($_GET['when']){
+		echo pageToJson(getPageUrl($_GET['when']));
+	}else{
+		echo pageToJson(getPageUrl());
+	}
+}else{
+	echo indexToJson(getUrl());
+}
